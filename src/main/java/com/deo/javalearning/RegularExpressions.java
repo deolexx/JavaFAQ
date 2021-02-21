551a93468c88739553ee57348d072722ce132c91
Deo <lukyanenko88@gmail.com>
2021-02-21 07:41:46 +0200

    more regex added

--- src/main/java/com/deo/javalearning/RegularExpressions.java
+++ src/main/java/com/deo/javalearning/RegularExpressions.java
@@ -1,5 +1,7 @@
 package com.deo.javalearning;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,7 +19,8 @@ public class RegularExpressions {
             System.out.printf("%s not match: %s%n", pattern, matcher);
         }
     }
-    static void find(String pattern,String  matcher) {
+
+    static void find(String pattern, String matcher) {
         Pattern pat = Pattern.compile(pattern);
         Matcher mat = pat.matcher(matcher);
 
@@ -27,28 +30,53 @@ public class RegularExpressions {
             System.out.printf("%s sequence not found: %s%n", pattern, matcher);
         }
     }
-    static void start(String pattern, String  matcher) {
+
+    static void start(String pattern, String matcher) {
         Pattern pat = Pattern.compile(pattern);
         Matcher mat = pat.matcher(matcher);
 
-        while(mat.find()) {
-            System.out.printf("%s sequence find at : %d%n", pattern,mat.start());
+        while (mat.find()) {
+            System.out.printf("%s sequence find at : %d%n", pattern, mat.start());
         }
     }
-    static void group(String pattern, String  matcher) {
+
+    static void group(String pattern, String matcher) {
         Pattern pat = Pattern.compile(pattern);
         Matcher mat = pat.matcher(matcher);
 
-        while(mat.find()) {
-            System.out.printf("%s sequence find at : %s%n", pattern,mat.group());
+        while (mat.find()) {
+            System.out.printf("%s sequence find at : %s%n", pattern, mat.group());
         }
     }
+
+    static void replaceAll(String pattern, String matcher, String replacement) {
+        Pattern pat = Pattern.compile(pattern);
+        Matcher mat = pat.matcher(matcher);
+        System.out.println(mat.replaceAll(replacement));
+    }
+
+    static void split(String pattern, String matcher) {
+        Pattern pat = Pattern.compile(pattern);
+        Matcher mat = pat.matcher(matcher);
+        String[] result = mat.pattern().split(matcher);
+        for (String s : result) System.out.println("next part: " + s);
+
+    }
+
+    static void matchesBool(String pattern, String matcher) {
+
+        System.out.println(Pattern.matches(pattern, matcher));
+    }
+
 
     public static void main(String[] args) {
         matches("Java", "Java 9");
         find("Java", "Java 9");
         start("a", "Java 9");
         group("W+", "W WW WWW"); // try diff quantifiers ? * +
+        replaceAll("J.+?n ","Jon Jonathan Mark Bill","Bob ");
+        split("[,. !]+","Hy, check!this,thing.is   !super,,,,,cool");
+        matchesBool("H.y", "Hey");
 
     }
 
