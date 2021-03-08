f459df874bb66e854deb7ac96378baf4066860ba
Deo <lukyanenko88@gmail.com>
2021-03-08 11:11:36 +0200

    lambdas

---
+++ src/main/java/com/deo/javalearning/collections/StackDemo.java
@@ -0,0 +1,12 @@
+package com.deo.javalearning.collections;
+
+import java.util.Stack;
+
+public class StackDemo {
+    Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+    }
+}
