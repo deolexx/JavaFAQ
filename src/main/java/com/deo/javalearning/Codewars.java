4082b79d8cbb83d6cc407f3f7fcb6fa96a511c8e
Deo <lukyanenko88@gmail.com>
2021-03-13 22:53:29 +0200

    mode codewars code added

--- src/main/java/com/deo/javalearning/Codewars.java
+++ src/main/java/com/deo/javalearning/Codewars.java
@@ -1,36 +1,85 @@
 package com.deo.javalearning;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 public class Codewars {
 
-    public static String sortMyString(String s) {
-        var ar1=new ArrayList();
-        var ar2=new ArrayList();
-        ar2.add(" ");
 
-        for(int i =0;i<s.length();i++){
-            if(i%2!=0){
-                ar2.add(s.charAt(i));
+    public static List<Integer> sumConsecutives(List<Integer> s) {
+        // your code
+        int sum=1;
+        for (int i = 0; i < s.size(); i++) {
+            if (s.get(i)==s.get(i-1)){
+               sum++;
+
             }
-            else ar1.add(s.charAt(i));
-        }
-        var res = new StringBuilder();
+            else if (sum>1){
 
-        for (var z : ar1) {
-            res.append(z);
-        }
-        for (var z : ar2) {
-            res.append(z);
+            }
+            else sum=1;
         }
 
 
-        return res.toString();
+
+        return s;
+    }
+
+    public static int[] sumParts(int[] ls) {
+        // your code
+        int sum = Arrays.stream(ls).sum();
+        int[] result = new int[ls.length+1];
+       for(int i=0;i<ls.length;i++){
+           result[i]= sum;
+           sum=sum-ls[i];
+       }
+    return result;
+
+
+    }
+
+    public static int findDifference(final int[] firstCuboid, final int[] secondCuboid) {
+        //your code here !!
+        int first = Arrays.stream(firstCuboid).reduce(1, (x, y) -> x * y);
+        int second = Arrays.stream(secondCuboid).reduce(1, (x, y) -> x * y);
+        return first > second ? first : second;
+    }
+
+
+    public static int reverse_bits(int n) {
+        //Your code here :)
+        return Integer.parseInt(new StringBuilder(Integer.toBinaryString(n)).reverse().toString(), 2);
+    }
+
+    public static boolean detect(String comment) {
+        // your code
+        return comment.matches("^(Can someone explain).*");
+
+
+    }
+
+    public static String printerError(String s) {
+        // your code
+        int count = 0;
+        Pattern pat = Pattern.compile("[^a-m]");
+        Matcher mat = pat.matcher(s);
+        while (mat.find()) count++;
+        return count + "";
+    }
+
+
+    public static int mixedSum(List<?> mixed) {
+
+        return mixed.stream().map(x -> x + "").flatMapToInt(s -> IntStream.of(Integer.parseInt(s))).sum();
+
     }
 
     public static void main(String[] args) {
 
-        System.out.println(sortMyString("Wolfeschlegelsteinhausenbergerdorff"));
+        System.out.println(sumParts(new int []{1,2,3,4,5}));
+
     }
 
 
