125d63d574f9baa50af83d4c362eccbbaed59944
Deo <lukyanenko88@gmail.com>
2021-05-20 21:15:07 +0300

    some basic stuff

--- src/main/java/com/deo/javalearning/ArrayDemo.java
+++ src/main/java/com/deo/javalearning/ArrayDemo.java
@@ -9,5 +9,10 @@ public class ArrayDemo {
         a = new int[4];
 
         int[] c = {1, 2, 3};   //instant initialization
+
+        int arr1 [] = new int[3]; // same!
+        int[] arr2 = new int[3]; //same!
+
+        char[] arr3,arr4,arr5; //multiple declaration
     }
 }
