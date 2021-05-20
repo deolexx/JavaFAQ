5ae69271097ab77c08fbac373479852fa14b3f7b
Deo <lukyanenko88@gmail.com>
2021-05-20 21:10:55 +0300

    some basic stuff

---
+++ src/main/java/com/deo/javalearning/methodsOverload/Birds.java
@@ -0,0 +1,7 @@
+package com.deo.javalearning.methodsOverload;
+
+public class Birds {
+    public void walk(){
+        System.out.println("Clap clap clap");
+    }
+}
---
+++ src/main/java/com/deo/javalearning/methodsOverload/Eagle.java
@@ -0,0 +1,8 @@
+package com.deo.javalearning.methodsOverload;
+
+public class Eagle extends Birds {
+    @Override
+    public void walk() {
+        System.out.println("Creeee");
+    }
+}
