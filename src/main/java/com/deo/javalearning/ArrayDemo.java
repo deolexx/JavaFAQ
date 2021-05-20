89ec352bdd3d7cca33f649783f2fc63ed70ec467
Deo <lukyanenko88@gmail.com>
2021-05-20 18:55:49 +0300

    some basic stuff

---
+++ src/main/java/com/deo/javalearning/ArrayDemo.java
@@ -0,0 +1,13 @@
+package com.deo.javalearning;
+
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+
+        int[] a;
+        a = new int[4];
+
+        int[] c = {1, 2, 3};   //instant initialization
+    }
+}
---
+++ src/main/java/com/deo/javalearning/Variables.java
@@ -0,0 +1,9 @@
+package com.deo.javalearning;
+
+public class Variables {
+    int a = 123__456;
+    int b = 1_2_3;
+    byte c = 12;
+    int d = c;
+    byte g =(byte) d;
+}
