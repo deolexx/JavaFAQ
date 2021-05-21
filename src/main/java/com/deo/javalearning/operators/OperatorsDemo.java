04d81914a810c667162e5c98162c92a9976d3e8b
Deo <lukyanenko88@gmail.com>
2021-05-21 21:23:26 +0300

    some basic stuff

---
+++ src/main/java/com/deo/javalearning/operators/OperatorsDemo.java
@@ -0,0 +1,21 @@
+package com.deo.javalearning.operators;
+
+public class OperatorsDemo {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <10; i++){
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    System.out.println(k);
+                }
+                System.out.println(j);
+            }
+            System.out.println(i);
+
+
+        }
+
+
+    }
+}
