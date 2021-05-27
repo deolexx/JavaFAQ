3abe9d5d90b2d0c7d5b772ffc4ccc2fd3fc508ae
Deo <lukyanenko88@gmail.com>
2021-05-27 14:38:53 +0300

    interfaces

---
+++ src/main/java/com/deo/javalearning/interfaces/InterfacesDemo.java
@@ -0,0 +1,12 @@
+package com.deo.javalearning.interfaces;
+
+public class InterfacesDemo {
+    public static void main(String [] args) {
+        A a = new TestClass();
+        a.methWithBody();
+
+        TestClass t = new TestClass();
+        t.testMeth();
+    }
+}
+
