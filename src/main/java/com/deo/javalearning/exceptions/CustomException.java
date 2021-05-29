e9ed59e603394cde3dd771cf586f662c779da198
Deo <lukyanenko88@gmail.com>
2021-05-29 16:36:22 +0300

    exceptions chains

---
+++ src/main/java/com/deo/javalearning/exceptions/CustomException.java
@@ -0,0 +1,19 @@
+package com.deo.javalearning.exceptions;
+
+public class CustomException extends Throwable{
+
+    @Override
+    public String toString() {
+        return "Чтото непонятное случилось";
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            throw new CustomException();
+        } catch (CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
