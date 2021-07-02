278b3966d0516b85d012158cec7b4033b7f8555d
Deo <lukyanenko88@gmail.com>
2021-07-02 12:32:00 +0300

    lambda expressions

---
+++ src/main/java/com/deo/javalearning/lambdaExpressions/LambdaDemo.java
@@ -0,0 +1,24 @@
+package com.deo.javalearning.lambdaExpressions;
+//Simple lambda expression demo
+
+//Functional interface (Single Abstract Method)
+interface MyNumber{
+    double getValue();
+}
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        MyNumber myNum; //create reference to the interface
+
+       myNum = () -> 123.45;
+
+        System.out.println(myNum.getValue());
+
+
+       myNum = () -> Math.random() * 100;
+
+        System.out.println(myNum.getValue());
+    }
+
+
+}
