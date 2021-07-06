2eb0f4acf20d12a511aa65de5ca2854acba43f39
Deo <lukyanenko88@gmail.com>
2021-07-06 12:27:57 +0300

    Calendar class minimum info added

---
+++ src/main/java/com/deo/javalearning/date/CalendarDemo.java
@@ -0,0 +1,17 @@
+package com.deo.javalearning.date;
+
+import java.util.Calendar;
+
+public class CalendarDemo {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+
+
+        System.out.println(calendar.get(Calendar.DATE));
+        System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+        System.out.println(calendar.get(Calendar.DAY_OF_YEAR));
+
+
+
+    }
+}
