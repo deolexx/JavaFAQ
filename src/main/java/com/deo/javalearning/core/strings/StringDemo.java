dc13146d055603439a0b24049f80a25c96330c67
Deo <lukyanenko88@gmail.com>
2021-07-12 11:44:59 +0300

    more examples added to StringDemo class

--- src/main/java/com/deo/javalearning/core/strings/StringDemo.java
+++ src/main/java/com/deo/javalearning/core/strings/StringDemo.java
@@ -2,20 +2,34 @@ package com.deo.javalearning.core.strings;
 
 public class StringDemo {
     public static void main(String[] args) {
-        //new string object
-        String s = new String(); //null
-        //creating string from char array
-        char[] chars = {'a','b','c','d','e','f'};
-        String s1 = new String(chars); //"abcdef"
-        //string from char array with offset and count
-        String s2 = new String(chars,2,2); //"cd"
-        //one String object from another
-        String s3 = new String(s2);
-        //String from byte array
-        byte [] ascii= {65,66,67,68,69};
-        String s4 = new String(ascii); //"ABCDE"
-        //NOTE!
-        //array values copy when the new string created form array,
-        //so if you change the array after than, string wont change
+
+
+            //new string object
+            String s = new String(); //null
+            //creating string from char array
+            char[] chars = {'a', 'b', 'c', 'd', 'e', 'f'};
+            String s1 = new String(chars); //"abcdef"
+            //string from char array with offset and count
+            String s2 = new String(chars, 2, 2); //"cd"
+            //one String object from another
+            String s3 = new String(s2);
+            //String from byte array
+            byte[] ascii = {65, 66, 67, 68, 69};
+            String s4 = new String(ascii); //"ABCDE"
+            //NOTE!
+            //array values copy when the new string created form array,
+            //so if you change the array after than, string wont change
+
+            //short string creation
+            String s5 = "abc";
+            System.out.println(s5.length()); //returns 3
+            System.out.println("abc".length()); //returns 3
+            //string concat
+            //with sting
+            String s6 = "a"+"b"+"c"; //"abc"
+            //with int
+            String s7 = "a"+2; // "a2" string will be created
+
+
     }
 }
